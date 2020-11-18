7e5b0413c1a2c39e7d22b4e430c018fc4b60e061
CrewHack <stu@allied-techs.com>
2020-11-17 21:06:02 -1000

    update reinstall

--- pages/api/performance.js
+++ pages/api/performance.js
@@ -38,10 +38,11 @@ export default async function foo(req, res) {
     {product: 'chrome'}
   );*/
   
-  const browser = await puppeteer.launch({
+  const browser = await puppeteer.launch(); 
+  /*{
     executablePath: process.env.PUPPETEER_EXEC_PATH,
     args: ['--no-sandbox']
-  });
+  }*/
   
   //const page = await browser.newPage();
   //await page.goto('https://example.com'); // change to your website
