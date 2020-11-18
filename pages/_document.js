b28b1ec6acf8d552fa505427fccbe8f1f89d9698
CrewHack <stu@allied-techs.com>
2020-11-18 12:53:40 -1000

    lighthouse performance tweaks

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-        <meta name="description" content="App description." />
+        <meta name="description" content="App description" />
         </Head>
         <body>
           <Main />
