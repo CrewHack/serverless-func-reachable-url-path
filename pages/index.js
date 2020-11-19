e28cb9931144fd426379764a1ab49ec403346a9a
CrewHack <stu@allied-techs.com>
2020-11-18 22:02:06 -1000

    pwa stuffz

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,7 @@ export default function Home(props) {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">My PWA</a>
+          Welcome to <a href="https://nextjs.org">my PWA</a>
         </h1>
 
         <strong>{myFetchedData}</strong>
