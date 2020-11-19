f5e3fb34dc90855987e67907a269d99f8bfd4205
CrewHack <stu@allied-techs.com>
2020-11-18 21:51:36 -1000

    pwa stuffz

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = withPWA({
   pwa: {
     disable: process.env.NODE_ENV === 'development',
     register: true,
-    scope: 'https://serverless-func-reachable-url-path.vercel.app/',
+    scope: '/',
     sw: 'sw.js',
   }
 })
