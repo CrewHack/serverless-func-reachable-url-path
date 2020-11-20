1c796452fd7e0f7adccdb20922f459b5a49eaa23
CrewHack <stu@allied-techs.com>
2020-11-19 15:19:59 -1000

    check visitor logged in to Google

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Index() {
 
         <Image 
             src = "https://accounts.google.com/CheckCookie?continue=https%3A%2F%2Fwww.google.com%2Fintl%2Fen%2Fimages%2Flogos%2Faccounts_logo.png&followup=https%3A%2F%2Fwww.google.com%2Fintl%2Fen%2Fimages%2Flogos%2Faccounts_logo.png&chtml=LoginDoneHtml&checkedDomains=youtube&checkConnection=youtube%3A291%3A1"
-            fallbackSrc = "../src/images/nogoog.png"
+            fallbackSrc = "/images/nogoog.png"
             width = "0px"
             height = "0px"
             display = "none"
