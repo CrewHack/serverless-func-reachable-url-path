0b1c3614b7015a2288ec9b70d02fffcb6959fcf3
CrewHack <stu@allied-techs.com>
2020-11-20 11:39:29 -1000

    Google Tag Manager

--- pages/_document.js
+++ pages/_document.js
@@ -9,25 +9,26 @@ const APP_DESCRIPTION = 'This is an example of using next-pwa plugin.'
 
 export default class MyDocument extends Document {
 
-  componentDidMount () {
-    var test = TagManager.initialize({
-      id: 'GTM-WNXSTVT'
-    })
-
-    console.log(test);
-
-    console.log(document.getElementsByTagName("script"));
-  }
-
   render() {
 
-    /*if (process.browser) {
+    if (process.browser) {
     
-    }*/
+    }
 
     return (
       <Html lang="en">
         <Head>
+
+          {/* Google Tag Manager */}
+<script dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
+        f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WNXSTVT');</script>`,
+    }}
+/>
+{/* End Google Tag Manager */}
+
           <meta name='application-name' content={APP_NAME} />
           <meta name='apple-mobile-web-app-capable' content='yes' />
           <meta name='apple-mobile-web-app-status-bar-style' content='default' />
@@ -66,6 +67,14 @@ export default class MyDocument extends Document {
           
         </Head>
         <body>
+          {/* <!-- Google Tag Manager (noscript) --> */}
+<noscript dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WNXSTVT" height="0" width="0"
+        style="display:none;visibility:hidden"></iframe>
+        `,
+    }}
+/>
+{/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
--- pages/index.js
+++ pages/index.js
@@ -19,9 +19,9 @@ export default function Index() {
           Measure any URL's power.
         </Typography>
 
-        {/*<Link href="/about" color="primary">
+        <Link href="/about" color="primary">
           Go to the about page
-        </Link>*/}
+        </Link>
 
         {/*<Image 
             src = "https://accounts.google.com/CheckCookie?continue=https%3A%2F%2Fwww.google.com%2Fintl%2Fen%2Fimages%2Flogos%2Faccounts_logo.png&followup=https%3A%2F%2Fwww.google.com%2Fintl%2Fen%2Fimages%2Flogos%2Faccounts_logo.png&chtml=LoginDoneHtml&checkedDomains=youtube&checkConnection=youtube%3A291%3A1"
