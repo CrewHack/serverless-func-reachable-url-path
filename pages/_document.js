9f82eedf28b204e68add871a30519cd95aa57d59
CrewHack <stu@allied-techs.com>
2020-11-20 12:05:39 -1000

    Google Tag Manager

--- pages/_document.js
+++ pages/_document.js
@@ -10,24 +10,21 @@ const APP_DESCRIPTION = 'This is an example of using next-pwa plugin.'
 export default class MyDocument extends Document {
 
   render() {
-
-    if (process.browser) {
-    
-    }
-
     return (
       <Html lang="en">
         <Head>
 
-          {/* Google Tag Manager */}
-<script dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
-        f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WNXSTVT');</script>`,
-    }}
-/>
-{/* End Google Tag Manager */}
+        {/* Google Tag Manager HEAD snippet*/}
+        <script dangerouslySetInnerHTML={
+            {
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
+                f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-WNXSTVT');</script>`,
+            }
+        }
+        />
+        {/* End Google Tag Manager HEAD snippet*/}
 
           <meta name='application-name' content={APP_NAME} />
           <meta name='apple-mobile-web-app-capable' content='yes' />
@@ -67,14 +64,16 @@ export default class MyDocument extends Document {
           
         </Head>
         <body>
-          {/* <!-- Google Tag Manager (noscript) --> */}
-<noscript dangerouslySetInnerHTML={{
-        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WNXSTVT" height="0" width="0"
-        style="display:none;visibility:hidden"></iframe>
-        `,
-    }}
-/>
-{/* <!-- End Google Tag Manager (noscript) --> */}
+            {/* <!-- Google Tag Manager BODY snippet (noscript) --> */}
+            <noscript dangerouslySetInnerHTML={
+                {
+                    __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WNXSTVT" height="0" width="0"
+                    style="display:none;visibility:hidden"></iframe>
+                    `,
+                }
+            }
+           />
+           {/* <!-- End Google Tag Manager BODY snippet (noscript) --> */}
           <Main />
           <NextScript />
         </body>
