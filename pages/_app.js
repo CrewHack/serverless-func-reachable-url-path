a1a7b5fe377a93360bbd52846cbe5e7dcd988b6c
CrewHack <stu@allied-techs.com>
2020-11-20 09:50:00 -1000

    Google Tag Manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,46 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/theme';
-import { makeStyles } from '@material-ui/core/styles';
-import PrimarySearchAppBar from '../src/PrimarySearchAppBar';
+import App, { Container } from 'next/app'
+import React from 'react'
+import './app.css'
+import TagManager from 'react-gtm-module'
 
-const useStyles = makeStyles(theme => ({
-  offset: theme.mixins.toolbar,
-}))
-
-export default function MyApp(props) {
-  
-  const { Component, pageProps } = props;
-  const classes = useStyles();
-
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-
-  return (
-    <React.Fragment>
-      <Head>
-        <title>My page</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <PrimarySearchAppBar />
-        <div className={classes.offset} />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </React.Fragment>
-  );
+const tagManagerArgs = {
+  id: 'GTM-GTM-WNXSTVT'
 }
 
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
+class MyApp extends App {
+  componentDidMount () {
+    TagManager.initialize(tagManagerArgs)
+  }
+
+  render () {
+    const { Component, pageProps } = this.props
+    return (
+      <Container>
+        Hi
+      </Container>
+    )
+  }
+}
+
+export default MyApp
