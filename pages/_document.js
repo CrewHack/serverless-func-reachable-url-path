1a7aeb8c856a22bdfcf703415bc683d0b560c7c4
CrewHack <stu@allied-techs.com>
2020-11-20 11:22:13 -1000

    Google Tag Manager

--- pages/_document.js
+++ pages/_document.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
 import theme from '../src/theme';
+import TagManager from 'react-gtm-module'
 
 const APP_NAME = 'next-pwa example'
 const APP_DESCRIPTION = 'This is an example of using next-pwa plugin.'
 
 export default class MyDocument extends Document {
   render() {
+
+    TagManager.initialize({
+      id: 'GTM-WNXSTVT'
+    })
+
     return (
       <Html lang="en">
         <Head>
