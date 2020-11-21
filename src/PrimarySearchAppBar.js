c4d0439fca773086e6e7bb762402149aa8fd0d37
CrewHack <stu@allied-techs.com>
2020-11-21 01:30:01 -1000

    style tweaks

--- src/PrimarySearchAppBar.js
+++ src/PrimarySearchAppBar.js
@@ -193,12 +193,12 @@ export default function PrimarySearchAppBar() {
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
             <IconButton aria-label="show 0 new mails" color="inherit">
-              <Badge badgeContent={4} color="#fdf731">
+              <Badge badgeContent={4} color="warning.light">
                 <MailIcon />
               </Badge>
             </IconButton>
             <IconButton aria-label="show 2 new notifications" color="inherit">
-              <Badge badgeContent={17} color="#fdf731">
+              <Badge badgeContent={17} color="warning.light">
                 <NotificationsIcon />
               </Badge>
             </IconButton>
--- src/theme.js
+++ src/theme.js
@@ -19,6 +19,9 @@ const theme = createMuiTheme({
     background: {
       default: '#fff',
     },
+    warning: {
+      light: '#fdf731',
+    }
   },
 });
 
