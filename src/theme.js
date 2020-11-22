de02296e3b447aa5f054e7973278294a6730b5c8
CrewHack <stu@allied-techs.com>
2020-11-21 19:10:07 -1000

    material button tweak

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ import { red, yellow, white} from '@material-ui/core/colors';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#22ac38', // #7FFF00' // #e93f33 // #1976d2
+      main: '#1976d2', // #7FFF00' // #e93f33 // #1976d2 // #22ac38
     },
     secondary: {
       main: '#ee7e33', // #fdf731 // #65aff7
