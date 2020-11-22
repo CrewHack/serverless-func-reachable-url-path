76fa0f8ddc06b46377c8408fc3ad44e3018659c5
CrewHack <stu@allied-techs.com>
2020-11-21 19:34:12 -1000

    material tweaks

--- pages/_document.js
+++ pages/_document.js
@@ -67,11 +67,7 @@ export default class MyDocument extends Document {
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
 
-          {/*{fonts}*/}
-          <link rel="preconnect" href="https://fonts.gstatic.com">    
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet"/>          
-          {/*<noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/></noscript>*/}</link>
-          
+         
         </Head>
         <body>
             {/* <!-- Google Tag Manager BODY snippet (noscript) --> */}
