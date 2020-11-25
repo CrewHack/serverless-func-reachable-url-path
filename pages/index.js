1fe85f4ffe9c715f889f63301ee1263df017fde5
CrewHack <stu@allied-techs.com>
2020-11-24 15:29:24 -1000

    main form updates

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,11 @@ function Index(props) {
 
   const { classes } = props;
 
-  const drawerToggle = (event) => { 
+  const drawerToggle = (e) => { 
+
+    e.preventDefault(); e.stopPropagation();
+    window.scrollTo(0,0); //the second 0 marks the Y scroll pos. Setting this to i.e. 100 will push the screen up by 100px. 
+
     setOpen(!open);
   };
 
