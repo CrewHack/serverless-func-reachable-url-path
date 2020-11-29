73b7b3bc99ac41d29141578372e1f5ad81831569
CrewHack <stu@allied-techs.com>
2020-11-28 14:54:29 -1000

    api work

---
+++ pages/backgroundVideo.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useRef} from "react"
+import Container from '@material-ui/core/Container';
+
+const BackgroundVideo = () => {
+  const videoRef = useRef()
+
+  useEffect(() => {
+    setTimeout(()=>{
+      videoRef.current.play()
+    },5000)
+  }, []);
+
+  return <Container maxWidth="lg">
+  <video
+      ref={videoRef}
+      controls
+      width="100%"
+      loop
+      muted
+      style={{
+        position: "relative",
+        width: "100%",
+        height: "100%",
+        left: 0,
+        top: 0,
+      }}>
+    <source src="/1_Getridox.mp4" type="video/mp4"/>
+  </video>
+</Container>
+}
+
+export default BackgroundVideo
