abb0162db556d87f6418f2f967afe6d521935b60
CrewHack <stu@allied-techs.com>
2020-12-30 12:59:54 -1000

    yo

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -58,7 +58,7 @@ export default function ThankYou() {
          That's it! Easy, isn't it? It's quick to implement, too. <b>You can get up & running accepting Bitcoin payment today.</b> <p>Need inspiration? Check out a real-world example of acceptBTC in action in an eCommerce shop - <i>look for the orange 'Buy with Bitcoin' button </i> on the product page. Their sales are skyrocketing as a result. Give it a try!</p>
         </Typography>
 
-        <div>&nbsp;</div>
+        {/*<div>&nbsp;</div>*/}
 
         <Button variant="contained" color="grey" component={Link} naked href="https://www.style-element.co/product/mens-silver-grey-classic-aviator-sunglasses?ref=acceptBTC" target="_blank">
           View Example Shop
