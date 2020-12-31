84c81be6ffd80f94ecad8eb6b9bc77a931fcca30
CrewHack <stu@allied-techs.com>
2020-12-30 16:13:42 -1000

    yo

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,8 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
 import theme from '../src/theme';
 
-const APP_NAME = 'next-pwa example'
-const APP_DESCRIPTION = 'This is an example of using next-pwa plugin.'
+const APP_NAME = 'acceptBTC'
+const APP_DESCRIPTION = 'This is the acceptBTC app.'
 
 export default class MyDocument extends Document {
 
@@ -23,7 +23,7 @@ export default class MyDocument extends Document {
                 __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
                 f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-WNXSTVT');</script>`,
+                })(window,document,'script','dataLayer','GTM-PMKP29S');</script>`, //GTM-WNXSTVT <- team@urlpow.com
             }
         }
         />
@@ -68,9 +68,9 @@ export default class MyDocument extends Document {
             {/* <!-- Google Tag Manager BODY snippet (noscript) --> */}
             <noscript dangerouslySetInnerHTML={
                 {
-                    __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WNXSTVT" height="0" width="0"
-                    style="display:none;visibility:hidden"></iframe>
-                    `,
+                    __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PMKP29S" height="0" width="0"
+                    style="display:none;visibility:hidden"></iframe> 
+                    `, //GTM-WNXSTVT <- team@urlpow.com
                 }
             }
            />
