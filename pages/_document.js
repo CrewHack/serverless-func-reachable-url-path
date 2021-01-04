a3da7e83921b6a9f81bf03e4fb03ff644d53f3d6
CrewHack <stu@allied-techs.com>
2021-01-04 11:20:23 -1000

    yo

--- pages/_document.js
+++ pages/_document.js
@@ -61,7 +61,8 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content={theme.palette.primary.main} />
 
           <link rel="preconnect" href="https://fonts.gstatic.com"/>    
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+          {/*<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet"/>*/}
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" media="none" onload="if(media!='all')media='all'"></link>
 
         </Head>
         <body>
