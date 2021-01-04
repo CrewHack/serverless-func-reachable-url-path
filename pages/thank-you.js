bdef1c3f58c07b4c69db3f3e000dec113aa22f8c
CrewHack <stu@allied-techs.com>
2021-01-03 14:10:08 -1000

    yo

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -3,7 +3,7 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import ProTip from '../src/ProTip';
+// import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
 import Image from 'next/image'
