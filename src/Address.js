807caf30ce8415feb3f3ab522c1107f06c2cbc6b
CrewHack <stu@allied-techs.com>
2021-01-03 16:36:26 -1000

    yo

---
+++ src/Address.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default function Address() {
+  return (
+    
+    <Typography variant="body2" color="textSecondary" align="center">
+        <address >
+        <div>1110 Nuuanu Ave</div>
+        <div>Suite 5043</div>
+        <div>Honolulu, HI 96817</div>
+        <div>USA</div>
+        
+        <div><a href="mailto:support@acceptbtc.co" target="_blank">support@acceptbtc.co</a></div>
+        
+        <a href="https://m.me/acceptbtc" target="_blank">m.me/acceptbtc</a>
+        </address>
+        <span>&nbsp;&nbsp;</span>
+    </Typography>
+  );
+}
