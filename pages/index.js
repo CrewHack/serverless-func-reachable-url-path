196a5c01bbd42f6218d1b4056eeaa2df9e70a697
CrewHack <stu@allied-techs.com>
2021-01-06 10:42:14 -1000

    yo

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,7 @@ import MainForm from '../src/MainForm';
 import Image from 'next/image'
 //import { ClassNames } from '@emotion/react';
 import MuiLink from '@material-ui/core/Link';
+import ReactPlayer from 'react-player';
 
 const styles = {
 
@@ -339,13 +340,19 @@ function Index(props) {
 
       </Box>
 
-      <Address />
-
       <div>&nbsp;</div>
+
+      <ReactPlayer url="external/btc-accepted-here.mp4" />
+
+<div>&nbsp;</div>
 
         <Typography style={{fontSize: "12px", textAlign: "center"}}>
         <i>Why do we offer this free educational service? </i> Bitcoin revolutionized "money" forever. Your business will profit from the cryptocurrency innovations now becoming more & more mainstream by the minute. The time has arrived to get paid in Bitcoin instead of traditional fiat currency variants. The 3 step method we will teach you is <i>by far</i> the quickest & easiest way to start accepting Bitcoin as a payment method in the year 2021. You don't have to be a coder to implement, anyone can configure the setup quickly. You can get started today. With our setup, you don't have to wait on the blockchain to complete processing the transactions and receive your crypo payment, nearly 97% of all transactions are approved almost INSTANTLY! We make accepting BTC payments just as easy as accepting traditional payments like credit and debit cards. Are you accepting Bitcoin payments yet? Time to to act <b>now ⏱️ </b>
         </Typography>
+
+      <div>&nbsp;</div>
+
+      <Address />
 
       <div>&nbsp;</div>
 
