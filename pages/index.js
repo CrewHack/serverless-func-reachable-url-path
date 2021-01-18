33b5a8f2c07deade4ccba133a0bf1e54fd6ed40d
CrewHack <stu@allied-techs.com>
2021-01-18 13:41:23 -1000

    yo

--- pages/index.js
+++ pages/index.js
@@ -297,22 +297,24 @@ function Index(props) {
           </div>
         )}
 
-        {/*{session && (
-          <>
-            <Typography>
-              <p>Thank you for being a member!</p>
+        {session && (
+          <div>
+            <>
+              <Typography>
+                <p>Thank you for being a member!</p>
 
-              <p>Please access the member's area: </p>
+                <p>Please access the member's area: </p>
 
-              <Button
-                href="/members"
-                style={{ color: "white", backgroundColor: "#FF9900" }}
-              >
-                Access Members Area
-              </Button>
-            </Typography>
-          </>
-        )}*/}
+                <Button
+                  //href="/members"
+                  style={{ color: "white", backgroundColor: "#FF9900" }}
+                >
+                  Access Members Area
+                </Button>
+              </Typography>
+            </>
+          </div>
+        )}
       </Box>
 
       <Typography style={{ fontSize: "11px", textAlign: "left" }}>
